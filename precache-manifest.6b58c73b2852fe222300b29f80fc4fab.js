7ee106dd5313898bfbdce8f51773e2d416b332ac
bananenwilly <pussinbootsdev@gmail.com>
2020-04-01 14:06:29 +0200

    Updates

---
+++ precache-manifest.6b58c73b2852fe222300b29f80fc4fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca826982fbdc63acb0470625bccf321",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67a78f83ff9b1a7fb4b0",
+    "url": "./static/css/main.8be815a9.chunk.css"
+  },
+  {
+    "revision": "c257da1bb302bff252b3",
+    "url": "./static/js/2.ac16f032.chunk.js"
+  },
+  {
+    "revision": "afcc9a3cd1714368ee6e417152216654",
+    "url": "./static/js/2.ac16f032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a78f83ff9b1a7fb4b0",
+    "url": "./static/js/main.56789865.chunk.js"
+  },
+  {
+    "revision": "7c5bacba9ec2476d63b1",
+    "url": "./static/js/runtime-main.3ae7674b.js"
+  }
+]);
